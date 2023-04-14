df12c53cf0de046f5b7a3ed8adc7207a3d53ae88
Ростислав <Master8G@yandex.ru>
2023-04-14 22:51:51 +0300

    Первая версия отображения объявлений.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -17,7 +18,12 @@ def index():
 
 @app.route('/search')
 def search():
-    return render_template('searchT.html', title='Поиск')
+    con = sqlite3.connect('db.db')
+    req = f"""Select name, id_person, description, price, data, id_files from adverts"""
+    cur = con.cursor()
+    ans = cur.execute(req).fetchall()
+    con.close()
+    return render_template('searchT.html', title='Поиск', adverts=ans)
 
 
 @app.route('/configuration')
@@ -28,6 +34,40 @@ def configuration():
 @app.route('/profile/<id>')
 def profile(id):
     return render_template('profileT.html', title='Поиск', id=id)
+
+
+def create():
+    try:
+        open('db.db', mode='r')
+    except Exception:
+        open('db.db', mode='w').close()
+        con = sqlite3.connect('db.db')
+        cur = con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS adverts(
+       id INT PRIMARY KEY,
+       name TEXT,
+       id_person INT,
+       description TEXT,
+       price TEXT, 
+       data DATE, 
+       id_files INT);
+    """)
+        con.commit()
+        cur.execute("""CREATE TABLE IF NOT EXISTS users(
+       id INT PRIMARY KEY,
+       name TEXT,
+       description TEXT,
+       address TEXT,  
+       password TEXT);
+    """)
+        con.commit()
+        cur.execute("""CREATE TABLE IF NOT EXISTS files(
+           id INT PRIMARY KEY,
+           name TEXT,
+           file BLOB);
+        """)
+        con.commit()
+        con.close()
 
 
 if __name__ == '__main__':
