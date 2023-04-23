c67d45600b3bfeefd220526e87e5a6a57e61c9fb
Ростислав <Master8G@yandex.ru>
2023-04-23 21:45:54 +0300

    упс....

--- data/files.py
+++ data/files.py
@@ -6,5 +6,6 @@ from .db_session import SqlAlchemyBase
 class File(SqlAlchemyBase):
     __tablename__ = 'files'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    advrt_id = sqlalchemy.Column(sqlalchemy.Integer)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     file = sqlalchemy.Column(sqlalchemy.BLOB, nullable=True)
